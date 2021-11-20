780bcd94a40ebd3597e5574ebed0f893797f754d
Radosław Reguła <radoslawregula@gmail.com>
2021-11-20 22:46:00 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='VoxG',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Singing voice synthesizer using GAN.',
+    author='Radosław Reguła',
+    license='MIT',
+)
