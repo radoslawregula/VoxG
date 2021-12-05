1432b57731fd2d3419d40783ced352cd4e3c098e
Radosław Reguła <radoslawregula@gmail.com>
2021-12-06 00:09:50 +0100

    process txt files, refactor in features.py

--- src/data/datasets.py
+++ src/data/datasets.py
@@ -6,17 +6,17 @@ import librosa
 import numpy as np
 import scipy
 
-from src.data.features import signal_to_features
+from src.data.features import Features
 
 
 logger = logging.getLogger(__name__)
 
 
 class RawDataPoint:
-    def __init__(self, txt: str, wav: str, id: str, is_sing: bool):
+    def __init__(self, txt: str, wav: str, id_numeric: str, is_sing: bool):
         self.txt = txt
         self.wav = wav
-        self.id = id
+        self.id_numeric = id_numeric
         self.is_sing = is_sing
 
 
@@ -45,19 +45,28 @@ class DataOrganizer:
         files_to_process = os.listdir(full_subpath)
         datapoints = list(set(map(lambda x: x.rsplit('.', maxsplit=1)[0],
                                   files_to_process)))
+        datapoints = sorted(datapoints)
         
         for dp in datapoints:
             txt = os.path.join(full_subpath, dp + '.txt')
             wav = os.path.join(full_subpath, dp + '.wav')
             is_sing = subpath == 'sing'
-            out_dpoints.append(RawDataPoint(txt, wav, id=dp, is_sing=is_sing))
+            out_dpoints.append(RawDataPoint(txt, wav, 
+                                            id_numeric=dp,
+                                            is_sing=is_sing))
         
         return out_dpoints
 
 
 class DataProcessor:
     def __init__(self, config: Dict):
-        self.sampling_rate = config['sampling_rate']
+        config_sampling = config['sampling']
+
+        self.sampling_rate = config_sampling['sampling_rate']
+        self.frame_len = config_sampling['frame_len']
+        self.hop_len = config_sampling['hop_len']
+
+        self.feats = Features()
 
     def preprocess(self, organizer: List[RawDataPoint]):
         """
@@ -67,7 +76,14 @@ class DataProcessor:
             logger.info(f'Processing file {num + 1}/{len(organizer)}...')
             audio, _ = librosa.load(point.wav, sr=self.sampling_rate, 
                                     mono=True, dtype=np.float64)
-            fourier = librosa.stft(audio, n_fft=1024, hop_length=256,
-                                   window=scipy.signal.windows.hann(1024))
+            fourier = librosa.stft(audio, n_fft=self.frame_len, 
+                                   hop_length=self.hop_len,
+                                   window=scipy.signal.windows.hann(self.frame_len))
             fourier = np.abs(fourier)
-            features = signal_to_features(audio, sampling_rate=self.sampling_rate)
+
+            features = self.feats.signal_to_features(audio, 
+                                                     sampling_rate=self.sampling_rate)
+            phonemes = self.feats.process_phonemes_file(point.txt, 
+                                                        sampling_rate=self.sampling_rate, 
+                                                        subframe=self.hop_len)
+            
--- src/data/features.py
+++ src/data/features.py
@@ -1,6 +1,9 @@
 import copy
+from math import ceil
+
 import librosa
 import numpy as np
+import pandas as pd
 import pysptk
 import pyworld as pw
 
@@ -8,37 +11,92 @@ from src.data.soundutils import frequency_to_pitch
 from src.utils.helpers import interpolate_inf
 
 
-def dimensionality_reduce(spectral_input: np.ndarray, n_dim: int, alpha: float, 
-                          noise_floor: float = -120.0) -> np.ndarray:
-    mcep = np.apply_along_axis(pysptk.mcep, axis=1, arr=spectral_input, 
-                               order=n_dim-1, alpha=alpha, maxiter=0, etype=1,
-                               eps=10**(noise_floor/10), min_det=0.0, itype=1)
-    # This part is taken directly from NPSS
-    scale_mcep = copy.copy(mcep)
-    scale_mcep[:, 0] *= 2
-    scale_mcep[:, -1] *= 2
-    mirror = np.hstack([scale_mcep[:, :-1], scale_mcep[:, -1:0:-1]])
-    mfsc = np.fft.rfft(mirror).real
+class Features:
+    COL_T_START = 't_start'
+    COL_T_END = 't_end'
+    COL_PHONEME = 'phoneme'
+    COL_REPEATS = 'repeats'
 
-    return mfsc
+    def __init__(self):
+        self.phonemes = []
+
+    @staticmethod
+    def dimensionality_reduce(spectral_input: np.ndarray, n_dim: int, alpha: float, 
+                            noise_floor: float = -120.0) -> np.ndarray:
+        mcep = np.apply_along_axis(pysptk.mcep, axis=1, arr=spectral_input, 
+                                order=n_dim-1, alpha=alpha, maxiter=0, etype=1,
+                                eps=10**(noise_floor/10), min_det=0.0, itype=1)
+        # This part is taken directly from NPSS
+        scale_mcep = copy.copy(mcep)
+        scale_mcep[:, 0] *= 2
+        scale_mcep[:, -1] *= 2
+        mirror = np.hstack([scale_mcep[:, :-1], scale_mcep[:, -1:0:-1]])
+        mfsc = np.fft.rfft(mirror).real
+
+        return mfsc
+
+    def signal_to_features(self, signal: np.ndarray, sampling_rate: float, 
+                           frame_period_samples: int = 256) -> np.ndarray:
+        frame_period = (frame_period_samples / sampling_rate) / 10**(-3)  # in miliseconds 
+        f_zero, spectral_env, aperiod = pw.wav2world(signal,
+                                                    sampling_rate,
+                                                    fft_size=1024,
+                                                    frame_period=frame_period)
+        aperiod = librosa.amplitude_to_db(aperiod)
+        spectral_env = librosa.power_to_db(spectral_env)
+
+        spectral_env = self.dimensionality_reduce(spectral_env, n_dim=60, alpha=0.45)
+        aperiod = self.dimensionality_reduce(aperiod, n_dim=4, alpha=0.45)
+        
+        with np.errstate(divide='ignore'):
+            f_zero, interpolation_mask = interpolate_inf(frequency_to_pitch(f_zero)) 
+
+        features = np.hstack((spectral_env, aperiod, f_zero, interpolation_mask))  
+
+        return features
+
+    @staticmethod
+    def _read_phoneme_file(txt_file: str) -> pd.DataFrame:
+        return pd.read_csv(txt_file, sep=' ', header=None, 
+                        names=['t_start', 't_end', 'phoneme'])
+
+    @staticmethod
+    def _seconds_to_subframe_idx(sec: float, sr: float, subframe: int) -> int:
+        return ceil((sec * sr) / subframe)
     
+    # TODO: inspect why that func is used
+    @staticmethod
+    def _substitute(phoneme: str) -> str:
+        return 'Sil' if phoneme in ('sil', 'br', 'pau') else phoneme
 
-def signal_to_features(signal: np.ndarray, sampling_rate: float, 
-                       frame_period_samples: int = 256) -> np.ndarray:
-    frame_period = (frame_period_samples / sampling_rate) / 10**(-3)  # in miliseconds 
-    f0, spectral_env, aperiod = pw.wav2world(signal,
-                                             sampling_rate,
-                                             fft_size=1024,
-                                             frame_period=frame_period)
-    aperiod = librosa.amplitude_to_db(aperiod)
-    spectral_env = librosa.power_to_db(spectral_env)
+    def process_phonemes_file(self, txt_file: str, sampling_rate: float, 
+                              subframe: int = 256) -> np.ndarray:
+        df = self._read_phoneme_file(txt_file)
+        col_subset = [self.COL_T_START, self.COL_T_END]
+        df[col_subset] = df[col_subset].applymap(lambda x: (
+            self._seconds_to_subframe_idx(x, 
+                                          sr=sampling_rate, 
+                                          subframe=subframe)
+        ))
+        df[self.COL_PHONEME] = df[self.COL_PHONEME].apply(self._substitute)
+        phonemes_here = df[self.COL_PHONEME].unique().tolist()
 
-    spectral_env = dimensionality_reduce(spectral_env, n_dim=60, alpha=0.45)
-    aperiod = dimensionality_reduce(aperiod, n_dim=4, alpha=0.45)
+        if not self.phonemes:
+            self.phonemes.extend(phonemes_here)
+        else:
+            self.phonemes.extend([pho for pho in phonemes_here 
+                                  if pho not in self.phonemes])
+        
+        df[self.COL_PHONEME] = df[self.COL_PHONEME].apply(self.phonemes.index)
+        df[self.COL_REPEATS] = df[self.COL_T_END].sub(df[self.COL_T_START])
+
+        phoneme_per_subframe = np.concatenate(
+            [np.repeat(row[self.COL_PHONEME], 
+                       row[self.COL_REPEATS]) for _, row in df.iterrows()]
+        )
+
+        return phoneme_per_subframe
+        
+
+
     
-    with np.errstate(divide='ignore'):
-        f0, interpolation_mask = interpolate_inf(frequency_to_pitch(f0)) 
-
-    features = np.hstack((spectral_env, aperiod, f0, interpolation_mask))  
-
-    return features
